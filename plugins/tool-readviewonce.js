8147d8ee355d596fe37c9911570a829c87dc13c8
ShionMDv <110561925+ShionMDv@users.noreply.github.com>
2022-08-18 04:10:15 +0700

    Update tool-readviewonce.js

--- plugins/tool-readviewonce.js
+++ plugins/tool-readviewonce.js
@@ -1,13 +1,23 @@
+let { downloadContentFromMessage } = require('@adiwajshing/baileys')
+
 let handler = async (m, { conn }) => {
-    if (!m.quoted) throw 'where\'s message?'
-    if (m.quoted.mtype !== 'viewOnceMessage') throw 'Itu bukan pesan viewOnce'
-    const msg = await conn.loadMessage(m.quoted.id)
-    if (!msg) throw 'can\'t open message!'
-    await conn.copyNForward(m.chat, msg, true, { readViewOnce: true })
+    if (!m.quoted) throw `Balas pesan viewOnce nya!`
+    if (m.quoted.mtype !== 'viewOnceMessage') throw 'Yang kamu balas bukan pesan viewOnce'
+    let type = Object.keys(m.quoted.message)[0]
+    let q = m.quoted.message[type]
+    let media = await downloadContentFromMessage(q, type == 'imageMessage' ? 'image' : 'video')
+    let buffer = Buffer.from([])
+    for await (const chunk of media) {
+        buffer = Buffer.concat([buffer, chunk])
+    }
+    if (/video/.test(type)) {
+        return conn.sendFile(m.chat, buffer, 'media.mp4', q.caption || '', m)
+    } else if (/image/.test(type)) {
+        return conn.sendFile(m.chat, buffer, 'media.jpg', q.caption || '', m)
+    }
 }
-
 handler.help = ['readviewonce']
-handler.tags = ['tools']
-handler.command = /^readviewonce/i
+handler.tags = ['group']
+handler.command = /^((read)?viewonce)$/i
 
-export default handler
+module.exports = handler
